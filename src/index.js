3a9a1e19ae00feaaec5df9c19d56d81ca3d7637e
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-04-20 19:00:09 +0300

    Commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import './css/styles.css';
+import { fetchCountries } from './resp';
+import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.js-list');
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+searchBox.addEventListener('input', debounce(handleSearch, 300));
+
+async function handleSearch() {
+  const searchTerm = searchBox.value.trim();
+
+  if (searchTerm === '') {
+    countryList.innerHTML = '';
+    return;
+  }
+
+  try {
+    const countries = await fetchCountries(searchTerm);
+
+    const countryItems = countries
+      .map(country => {
+        const flagSrc = country.flags?.svg ?? '';
+        const countryName = country.name?.common ?? '';
+        const alpha3Code = country.cca3 ?? '';
+        return `
+        <li class="country-item" data-country-code="${alpha3Code}">
+          <img src="${flagSrc}" alt="${countryName} flag" class="country-flag" />
+          <h2 class="country-name">${countryName}</h2>
+        </li>
+      `;
+      })
+      .join('');
+
+    countryList.innerHTML = countryItems;
+  } catch (error) {
+    console.error(error);
+  }
+}
---
+++ src/resp.js
@@ -0,0 +1,18 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+export function fetchCountries(name) {
+  const url = `${BASE_URL}/name/${name}?fields=name,flags.svg;cca3`;
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
