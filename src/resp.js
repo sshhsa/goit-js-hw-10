59fd26d4902d12b0329a272e415c93a1eb49644b
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-04-21 17:16:08 +0300

    Update resp.js

--- src/resp.js
+++ src/resp.js
@@ -1,18 +1,31 @@
-const BASE_URL = 'https://restcountries.com/v3.1';
+// const BASE_URL = 'https://restcountries.com/v3.1';
 
-export function fetchCountries(name) {
-  const url = `${BASE_URL}/name/${name}?fields=name,capital,flags,population,languages`;
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then(data => {
-      return data;
-    })
-    .catch(error => {
-      console.error(error);
-    });
+// export function fetchCountries(name) {
+//   const url = `${BASE_URL}/name/${name}?fields=name,capital,flags,population,languages`;
+//   return fetch(url)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(response.statusText);
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       return data;
+//     })
+//     .catch(error => {
+//       console.error(error);
+//     });
+// }
+
+function fetchCountries(name) {
+  const BASE_URL = 'https://restcountries.com/v3.1/name/';
+  return fetch(
+    `${BASE_URL}${name}?fields=name,capital,flags,population,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
 }
+export { fetchCountries };
